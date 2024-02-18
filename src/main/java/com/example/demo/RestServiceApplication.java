6fbc51d3ad4c64cf38faf1a9b93c13ad70589584
zhu2qian1 <zhu2qian1@gmail.com>
2024-02-18 23:37:53 +0900

    add example rest controller

--- src/main/java/com/example/demo/RestServiceApplication.java
+++ src/main/java/com/example/demo/RestServiceApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
-
+public class RestServiceApplication {
     public static void main(final String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(RestServiceApplication.class, args);
     }
 }
