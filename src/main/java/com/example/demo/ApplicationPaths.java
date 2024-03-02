e2a590139dab55003dba660b905a26de9bc8cc48
zhu2qian1 <zhu2qian1@gmail.com>
2024-03-03 01:56:51 +0900

    fix - unintended content-relative path -> relative path to root

--- src/main/java/com/example/demo/ApplicationPaths.java
+++ src/main/java/com/example/demo/ApplicationPaths.java
@@ -7,6 +7,6 @@ public final class ApplicationPaths {
 
     public static final class People {
         public static final String rel = "people";
-        public static final String path = "people";
+        public static final String path = "/people";
     }
 }
